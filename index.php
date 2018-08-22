2a6f9f470ef72b831f6ce54d4f2f287d1f78b827
Jonas Wrede <jonaswrede@aol.com>
2018-08-22 16:18:31 +0200

    session test

--- index.php
+++ index.php
@@ -21,7 +21,17 @@
         header("Location: login_page.php");
         exit;
     }
+$host_name = 'db748804796.db.1and1.com';
+$database = 'db748804796';
+$user_name = 'dbo748804796';
+$password = 'Tml321Bmg()=';
+$connect = mysqli_connect($host_name, $user_name, $password, $database);
 
+if (mysqli_connect_errno()) {
+    die('<p>Verbindung zum MySQL Server fehlgeschlagen: '.mysqli_connect_error().'</p>');
+} else {
+    echo '<p>Verbindung zum MySQL Server erfolgreich aufgebaut.</p >';
+}
 
     $pass = "Toshibalol80";
     $hash= '$1$QNiYVT2j$IQJ2wLmFZku/E5QC1LNJa/';
