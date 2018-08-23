33fa27368ee2f79f9b478f8cb5985be2958c2ac5
Jonas Wrede <jonaswrede@aol.com>
2018-08-23 14:08:35 +0200

    login setup 1

--- login_page.php
+++ login_page.php
@@ -9,7 +9,7 @@
 session_start();
 
 if($_SESSION["login"]){
-    header('Location: welcome.php');
+    header('Location: index.php');
 }
 
 ?>
@@ -24,25 +24,5 @@ if($_SESSION["login"]){
 </body>
 </html>
 
-
-<?php
-
-if(isset($_POST["login"])) {
-    if(empty($_POST["username"]) || empty($_POST["pass"])){
-        echo "<script> alert('Username or password not set!') </script>";
-    }
-    else{
-        $host_name = 'db748804796.db.1and1.com';
-        $database = 'db748804796';
-        $user_name = 'dbo748804796';
-        $password = 'Tml321Bmg()=';
-        $connect = mysqli_connect($host_name, $user_name, $password, $database);
-        $query = 'SELECT pass from login where lname='.$_POST(["username"]);
-
-        $result = $connect->query($query);
-
-        var_dump($result);
-    }
-}
 
 ?>
--- verify.php
+++ verify.php
@@ -1,13 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jonas.wrede
- * Date: 20.08.18
- * Time: 12:40
- */
 
-if(isset($_POST["login"]))
-    $_SESSION["logged"] = "yes";
+if(isset($_POST["login"])) {
+    if(empty($_POST["username"]) || empty($_POST["pass"])){
+        echo "<script> alert('Username or password not set!') </script>";
+    }
+    else{
+        $host_name = 'db748804796.db.1and1.com';
+        $database = 'db748804796';
+        $user_name = 'dbo748804796';
+        $password = 'Tml321Bmg()=';
+        $connect = mysqli_connect($host_name, $user_name, $password, $database);
+        $query = 'SELECT pass from login where lname='.$_POST(["username"]);
 
-var_dump($_SESSION);
+        $result = $connect->query($query);
 
+        var_dump($result);
+    }
+}
