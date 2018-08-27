0a170c9750aa5ea74905651e2a54ce1677e5ce3e
Jonas Wrede <jonaswrede@aol.com>
2018-08-27 12:01:45 +0200

    logout button

--- index.php
+++ index.php
@@ -6,3 +6,12 @@ if(!$_SESSION["login"])
     header("location: login_page.php");
 }
 ?>
+
+
+<html>
+    <body>
+        <form method ="POST" action="kill_session.php"
+              <input type="SUBMIT" value="Logout" name="loggout">
+        </form>
+    </body>
+</html>
