bae2b1d3c94b90243a4f65ae7752d2be32769184
Jonas Wrede <jonaswrede@aol.com>
2018-09-05 14:15:31 +0200

    handle file in log_creator

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ include "web_log_creator/log_creator.php"
                 <input type="SUBMIT" value="Logout" name="logout">
             </form>
             <?php
-            create_log("login");
+            create_log("login","jonas",100);
             if($_SESSION["usrgrp"]=="TSA"){
                 echo "Hello Admin";
                 ?>
