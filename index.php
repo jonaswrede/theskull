40b5c0d449950e1fd0dc2ebc5c86388b8efa969b
Jonas Wrede <jonaswrede@aol.com>
2018-09-06 14:06:25 +0200

    header (location) issue fixed (verify.php)

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
 session_start();
 
-?>
+if($_SESSION["login"]!= true)
+{
+    header("location: session/login_page.php");
+}
 
+include "web_log_creator/log_creator.php";
+
+?>
 
 
 <html>
@@ -12,30 +18,20 @@ session_start();
         </title>
     </head>
         <body>
-        <?php
-        if($_SESSION["login"]!= true)
-        {
-            echo"haha";
-        header("location: session/login_page.php");
-        }
-
-        include "web_log_creator/log_creator.php"
-        ?>
-
             <form method ="POST" action="session/kill_session.php">
                 <input type="SUBMIT" value="Logout" name="logout">
             </form>
             <?php
-            create_log("login","jonas",100);
-            if($_SESSION["usrgrp"]=="TSA"){
-                echo "Hello Admin";
-                ?>
-                <form method="post" action="user/create_user.php">
-                    <input type="Submit" value="Create User" name="create_user">
-                </form>
-            <?php
-            }
-            echo "<br> Session started at ". $_SESSION["time"];
+                create_log("login","jonas",100);
+                if($_SESSION["usrgrp"]=="TSA"){
+                    echo "Hello Admin";
+                    ?>
+                    <form method="post" action="user/create_user.php">
+                        <input type="Submit" value="Create User" name="create_user">
+                    </form>
+                <?php
+                }
+                echo "<br> Session started at ". $_SESSION["time"];
             ?>
         </body>
 </html>
