51acdd981d159b7ecbdfc3f4f3cc29a4f3909a3b
Jonas Wrede <jonaswrede@aol.com>
2018-09-10 18:56:24 +0200

    add log creator directly in verify.php

--- session/verify.php
+++ session/verify.php
@@ -45,7 +45,21 @@ ob_start();
                         $_SESSION["time"] = (string)$time;
                     }
 
-                    create_log("login",$uid,$uname,$status);
+                    #create_log("login",$uid,$uname,$status);
+
+                    $path = "web_logs/login_log.txt";
+                    $ip = $_SERVER["REMOTE_ADDR"];
+                    $log = "\n".$time.";".$uid.";".$user_name.";".$status.";".$ip;
+                    if(file_put_contents($path,$log, FILE_APPEND)){
+                        $_SESSION["log"] = "yes";
+                        ECHO $log;
+
+                    }
+                    else{
+                        $_SESSION["log"] = "no";
+                        $_SESSION["log_text"] = $log;
+                    };
+
                     header("location: ../index.php");
                 }
                 else{
