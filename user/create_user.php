6f329e0ca8bffccb7ea6e924200c67669d7916d0
Jonas Wrede <jonaswrede@aol.com>
2018-09-11 17:53:09 +0200

    START CREATE USER (ADD ADMIN Checkbox)

--- user/create_user.php
+++ user/create_user.php
@@ -7,35 +7,24 @@
  */
 
 session_start();
+#<?php
+
+if($_SESSION["login"]!=true || $_SESSION["usrgrp"]!="TSA") {
+    header("location: ../../index.php");
+}
 ?>
 
-    <html>
+<html>
     <title>
         Create User
     </title>
-
-<body>
-
-<?php
-
-if($_SESSION["login"]==true && $_SESSION["usrgrp"]=="TSA"  && isset($_POST["create_user"])){
-    ?>
-
+    <body>
         <h2>Create User</h2>
-            <form method="post" action="create.php">
-                <input type="text" placeholder="Username" name="user" required><br>
-                <input type="password" placeholder="Password" name="password" required><br>
-                <input type="submit" value="Create" name="create">
-            </form>
-
-<?php
-}
-else
-{
-    header("location: ../../index.php");
-}
-
-?>
-
-</body>
-    </html>
+        <form method="post" action="create.php">
+            <input type="text" placeholder="Username" name="user" required><br>
+            <input type="password" placeholder="Password" name="password" required><br>
+            <input type="checkbox" value="Admin" name="grp">
+            <input type="submit" value="Create" name="create">
+        </form>
+    </body>
+</html>
