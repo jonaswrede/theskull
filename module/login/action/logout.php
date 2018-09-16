eb5d3cff2097408b8138692b47c64e74888052c1
Jonas Wrede <jonaswrede@aol.com>
2018-09-16 14:27:31 +0200

    logout

--- module/login/action/logout.php
+++ module/login/action/logout.php
@@ -6,11 +6,13 @@
  * Time: 17:22
  */
 session_start();
+
 include "../../../web_logs/log_creator.php";
+
 create_log("logout",$_SESSION["uid"],$_SESSION["uname"],"150 Logout");
 
 session_destroy();
 
-header("location: ../index.php");
+header("location: ../../../index.php");
 
 ?>
