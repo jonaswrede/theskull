a0686a794584a3e7e12191f715bde843ca19db3c
Jonas Wrede <jonaswrede@aol.com>
2018-09-16 09:07:49 +0200

    test db

--- adapter/tsdb.php
+++ adapter/tsdb.php
@@ -25,9 +25,18 @@ use mysqli;
       * @return mysqli
       */
      public function __construct(){
-         $this->connect = new mysqli($this->host, $this->db, $this->user,$this->pw);
+         $host_name = 'db748804796.db.1and1.com';
+         $database = 'db748804796';
+         $user_name = 'dbo748804796';
+         $password = 'Tml321Bmg()=';
+         $connect = mysqli_connect($host_name, $user_name, $password, $database);
 
-         /** @var mysqli $connect */
+         if (mysqli_connect_errno()) {
+             die('<p>Verbindung zum MySQL Server fehlgeschlagen: '.mysqli_connect_error().'</p>');
+         } else {
+             echo '<p>Verbindung zum MySQL Server erfolgreich aufgebaut.</p >';
+         $this->connect=$connect;}
+
 
          return $this->connect;
     }
--- test_db.php
+++ test_db.php
@@ -1,13 +1,11 @@
 <?php
-$host_name = 'db748804796.db.1and1.com';
-$database = 'db748804796';
-$user_name = 'dbo748804796';
-$password = 'Tml321Bmg()=';
-$connect = mysqli_connect($host_name, $user_name, $password, $database);
 
-if (mysqli_connect_errno()) {
-    die('<p>Verbindung zum MySQL Server fehlgeschlagen: '.mysqli_connect_error().'</p>');
-} else {
-    echo '<p>Verbindung zum MySQL Server erfolgreich aufgebaut.</p >';
-}
+
+include "adapter/tsdb.php";
+
+$db = new \TSDBase\tsdb();
+
+
+
+
 ?>
