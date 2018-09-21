c2002e152ade4543feb268fa6d309d72fa168cc8
Jonas Wrede <jonaswrede@aol.com>
2018-09-21 13:44:32 +0200

    repare verify.php ?

--- module/login/action/verify.php
+++ module/login/action/verify.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include "../../../adapter/TSDb.php";
+include "../../../adapter/tsdb.php";
 include "../../../web_logs/log_creator.php";
 include "../../../module/user/class/User.php";
 /**
@@ -12,48 +12,48 @@ if(isset($_POST["login"]) || $_SESSION["login"]!= true)
         echo "<script> alert('Username or password not set!'); window.location.href='../view/login_page.php'</script>";
     }
     else{
-     #   $user = new User($_POST["username"],$_POST["pass"]);
-            $connect = new TSDb();
-            $result = $connect->select_user("SELECT lname,pass,id_group,id_user from login,user_to_group where lname= '" . $_POST['username'] . "' AND login.id = user_to_group.id_user");
-            $connect->close();
-            if (mysqli_num_rows($result)==1){
-                foreach ($result as $value){
-                    $pass = $value["pass"];
-                    $group = $value["id_group"];
-                    $uname = $value["lname"];
-                    $uid = $value["id_user"];
-                }
-                if(password_verify($_POST["pass"],$pass))
-                {
-                    $_SESSION["login"] = true;
-                    $_SESSION["uname"] = $uname;
-                    $_SESSION["uid"] = $uid;
-                    $status = "100 SUCCESS";
-                    if($group == "1"){
-                        $_SESSION["usrgrp"] = "TSA";
-                    }
-                    else{
-                        $_SESSION["usrgrp"]= "Standard User";
-                    }
-                    if(!$_SESSION["time"]) {
-                        $time = date('m/d/Y h:i:s a', time());
-                        $_SESSION["time"] = (string)$time;
-                    }
-                    create_log("login",$uid,$uname,$status);
-                    header("location: /index");
+        $user = new User($_POST["username"],$_POST["pass"]);
+        $connect = new TSDb();
+        $result = $connect->select_user("SELECT lname,pass,id_group,id_user from login,user_to_group where lname= '" . $_POST['username'] . "' AND login.id = user_to_group.id_user");
+        $connect->close();
+        if (mysqli_num_rows($result)==1){
+            foreach ($result as $value){
+                $pass = $value["pass"];
+                $group = $value["id_group"];
+                $uname = $value["lname"];
+                $uid = $value["id_user"];
+            }
+            if(password_verify($_POST["pass"],$pass))
+            {
+                $_SESSION["login"] = true;
+                $_SESSION["uname"] = $uname;
+                $_SESSION["uid"] = $uid;
+                $status = "100 SUCCESS";
+                if($group == "1"){
+                    $_SESSION["usrgrp"] = "TSA";
                 }
                 else{
-                    $status = "200 ERROR PWD";
-                    create_log("login",$uid,$uname,$status);
-                    echo
-                    "<script>alert('Credentials not correct !');window.location.href='../view/login_page.php';</script>";
+                    $_SESSION["usrgrp"]= "Standard User";
                 }
+                if(!$_SESSION["time"]) {
+                    $time = date('m/d/Y h:i:s a', time());
+                    $_SESSION["time"] = (string)$time;
+                }
+                create_log("login",$uid,$uname,$status);
+                header("location: /index");
             }
             else{
-                $status = "250 ERROR USR N.Exist";
-                echo "<script>alert('Credentials not correct !'); window.location.href='../view/login_page.php'</script>";
+                $status = "200 ERROR PWD";
+                create_log("login",$uid,$uname,$status);
+                echo
+                "<script>alert('Credentials not correct !');window.location.href='../view/login_page.php';</script>";
             }
         }
+        else{
+            $status = "250 ERROR USR N.Exist";
+            echo "<script>alert('Credentials not correct !'); window.location.href='../view/login_page.php'</script>";
+        }
+    }
 }
 else{
     echo"<script>alert('Please login'); window.location.href='../view/login_page.php'</script>";
