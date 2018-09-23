9e3395b0b352824ef3d9ee700a74756192e57643
Jonas Wrede <jonaswrede@aol.com>
2018-09-24 00:37:33 +0200

    ADD THE BULLET SKULLgit add --allgit add --all

--- module/login/view/login_page.php
+++ module/login/view/login_page.php
@@ -57,6 +57,7 @@ if($_SESSION["login"]){
         <div class="container-fluid border">
             <div class="row-fluid">
                 <div class="centering text-center">
+                    <img src="../../../bullet_idea.jpeg" height="100px" width="100px">
                     <form action="../action/verify.php"  method="post">
                         <input type="text" name="username" placeholder="Username" required><br>    <br>
                         <input type="password" name="pass" placeholder="Password" required><br> <br>
