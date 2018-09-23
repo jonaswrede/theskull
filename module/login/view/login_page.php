008d27dc6f0785a871b460368b86217f95f95c9e
Jonas Wrede <jonaswrede@aol.com>
2018-09-24 00:20:27 +0200

    ADD BOOTSSTRAP + FIRST DIV

--- module/login/view/login_page.php
+++ module/login/view/login_page.php
@@ -12,22 +12,36 @@ session_start();
 if($_SESSION["login"]){
     header('Location: /index.php');
 }
+?>
 
 
 ?>
 
 <html>
 <head>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <title>
         Login
     </title>
 </head>
     <body>
+    <div align="center" class="container">
         <form action="../action/verify.php"  method="post">
             <input type="text" name="username" placeholder="Username" required>
             <input type="password" name="pass" placeholder="Password" required>
             <input type="Submit" name="login" value="Login">
         </form>
+    </div>
     </body>
 </html>
 
