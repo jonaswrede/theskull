f887a3943211894a170cb90a03c20cc7967deb8b
Jonas Wrede <jonaswrede@aol.com>
2018-09-25 16:16:05 +0200

    index container

--- index.php
+++ index.php
@@ -11,11 +11,20 @@ include "web_logs/log_creator.php";
 ?>
 <html>
     <head>
-        <title>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+            <title>
             Index
         </title>
     </head>
         <body>
+        <div class="container">
             <form method ="POST" action="module/login/action/logout.php">
                 <input type="SUBMIT" value="Logout" name="logout">
             </form>
@@ -26,4 +35,5 @@ include "web_logs/log_creator.php";
                 echo "<br> Session started at ". $_SESSION["time"];
             ?>
         </body>
+    </div>
 </html>
