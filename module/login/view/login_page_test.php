3bfa27a2a4c24a8e17fd44aebf4cdb416a1e003e
Jonas Wrede <jonaswrede@aol.com>
2018-09-27 13:47:48 +0200

    update module->login->view->login_page_test.php test new symbols for forms (BS input-group)

--- module/login/view/login_page_test.php
+++ module/login/view/login_page_test.php
@@ -12,15 +12,14 @@
 <html>
 <head>
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
     <style>
-
+/**
         html, body{height:100%; margin:0;padding:0}
 
         .container-fluid{
@@ -28,9 +27,7 @@
             display:table;
             width: 100%;
             padding: 0;
-            /**  border: 5px solid gray;*/
-
-
+            /**  border: 5px solid gray;
         }
 
         .row-fluid {height: 100%; display:table-cell; vertical-align: middle;}
@@ -38,14 +35,14 @@
         .centering {
             float:none;
             margin:0 auto;
-        }
+        }**/
     </style>
 
     <title>
         Login
     </title>
 </head>
-<body STYLE="background-color: black">
+<body>
 
 <div class="container-fluid">
     <div class="row-fluid">
