dcc7a7f1751b1f8963023950a25ba2c9f9682efb
Jonas Wrede <jonaswrede@aol.com>
2018-09-27 16:24:52 +0200

    update module->login->view->login_page_test.php test new symbols for forms (BS input-group) change BS V

--- module/login/view/login_page_test.php
+++ module/login/view/login_page_test.php
@@ -57,11 +57,11 @@
                     <!-- Email -->
                     <div class="input-group">
                         <div class="input-group-prepend">
-                            <span class="input-group-text"><span class="oi oi-person"></span>
+                            <span class="input-group-text"><span class="oi oi-person"></span></span>
                         <input id="username" type="text" class="form-control m-4" name="username" placeholder="Username">
                     </div>
                     <div class="input-group p-5">
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                        <span class="input-group-addon"><span class="oi oi-lock-locked"></span></span>
                         <input id="password" type="password" class="form-control m-4" name="password" placeholder="Password">
                     </div>
                     <button class="btn btn-secondary m-5" type="submit">Sign in</button>
