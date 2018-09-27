5be0219cb18f4e5923f929624fbc9ef470e3b5ef
Jonas Wrede <jonaswrede@aol.com>
2018-09-27 13:52:46 +0200

    update module->login->view->login_page_test.php test new symbols for forms (BS input-group)

--- module/login/view/login_page_test.php
+++ module/login/view/login_page_test.php
@@ -75,42 +75,6 @@
         </div>
     </div>
 </div>
-<div class="container">
-    <h3>Input Groups</h3>
-    <p>The .input-group class is a container to enhance an input by adding an icon, text or a button in front or behind it as a "help text".</p>
-    <p>The .input-group-addon class attaches an icon or help text next to the input field.</p>
-
-    <form>
-        <div class="input-group">
-            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-            <input id="email" type="text" class="form-control" name="email" placeholder="Email">
-        </div>
-        <div class="input-group">
-            <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-            <input id="password" type="password" class="form-control" name="password" placeholder="Password">
-        </div>
-        <br>
-        <div class="input-group">
-            <span class="input-group-addon">Text</span>
-            <input id="msg" type="text" class="form-control" name="msg" placeholder="Additional Info">
-        </div>
-    </form>
-    <br>
-
-    <p>It can also be used on the right side of the input:</p>
-    <form>
-        <div class="input-group">
-            <input id="email" type="text" class="form-control" name="email" placeholder="Email">
-            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-        </div>
-        <div class="input-group">
-            <input id="password" type="password" class="form-control" name="password" placeholder="Password">
-            <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-        </div>
-    </form>
-</div>
-
-
 </body>
 </html>
 
