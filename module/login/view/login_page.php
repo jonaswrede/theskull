c3d933050dac981ba65197b6a7466d30dea0f717
Jonas Wrede <jonaswrede@aol.com>
2018-09-28 12:50:44 +0200

    update module->login->view->login_page_test.php test new symbols for forms (BS input-group) change BS V

--- module/login/view/login_page.php
+++ module/login/view/login_page.php
@@ -10,90 +10,82 @@
 ?>
 
 <html>
-<head>
-    <!-- Latest compiled and minified CSS -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../../ext/open-iconic-master/font/css/open-iconic-bootstrap.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <head>
+        <!-- Latest compiled and minified CSS -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../../ext/open-iconic-master/font/css/open-iconic-bootstrap.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-    <style>
+        <style>
 
-        html, body{height:100%; margin:0;padding:0; background-color:}
+            html, body{height:100%; margin:0;padding:0; background-color:black}
 
-        .container-fluid{
-            height:100%;
-            display:table;
-            width: 100%;
-            padding: 0;
-            /**  border: 5px solid gray;*/
-        }
+            .container-fluid{
+                height:100%;
+                display:table;
+                width: 100%;
+                padding: 0;
+                /**  border: 5px solid gray;*/
+            }
 
-        .row-fluid {height: 100%; display:table-cell; vertical-align: middle;}
+            .row-fluid {height: 100%; display:table-cell; vertical-align: middle;}
 
-        .centering {
-            float:none;
-            margin:0 auto;
-        }
+            .centering {
+                float:none;
+                margin:0 auto;
+            }
+        </style>
 
-        .center-block {
-            display: table;  /* Instead of display:block */
-            margin-left: auto;
-            margin-right: auto;
-        }
-    </style>
-
-    <title>
-        Login
-    </title>
-</head>
-<body style="background-color: black">
-
-<div class="container-fluid">
-    <div class="row-fluid">
-        <!-- <span class="border border border-dark">-->
-        <div class="centering text-center">
-            <div class="container">
-                <img src="../../../theskull.png">
-            </div>
-            <div class="container border border-dark" style="background-color: black">
-                <img src="../../../bullet_idea.jpeg" height="80px" width="80px" hspace="20" class="rounded-circle p-2" style="margin-top: 10px">
-                <!-- Default form login -->
-                <form class="text-center p-2" action="../action/verify.php" method="post">
-                    <p class="h4 mb-4">Sign in</p>
-                    <!-- Email -->
-                    <div>
-                        <div class="input-group mb-2">
-                            <div class="input-group-prepend">
-                                <div class="input-group-text pr-3" id="btnGroupAddon2"><i class="oi oi-person" style="width: 10px"></i></div>
-                            </div>
-                            <input class="form-control py-2 border" type="text" placeholder="Username">
-                        </div>
-                        <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                                <div class="input-group-text pr-3" id="btnGroupAddon2"><i class="oi oi-lock-locked" style="width:10px"></i></div>
-                            </div>
-                            <input class="form-control py-2 border" type="password" name="pass" placeholder="Password">
-                        </div>
-
+        <title>
+            Login
+        </title>
+    </head>
+    <body style="background-color: black">
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <!-- <span class="border border border-dark">-->
+                <div class="centering text-center">
+                    <div class="container">
+                        <img src="../../../theskull.png">
                     </div>
-                    <button class="btn btn-secondary mb-3" type="submit">Sign in</button>
+                    <div class="container border border-dark" style="background-color: black; max-width:75% ">
+                        <img src="../../../bullet_idea.jpeg" height="80px" width="80px" hspace="20" class="rounded-circle p-2" style="margin-top: 10px">
+                        <!-- Default form login -->
+                        <form class="text-center p-2" action="../action/verify.php" method="post">
+                            <p class="h4 mb-4">Sign in</p>
+                            <!-- Email -->
+                            <div>
+                                <div class="input-group mb-2">
+                                    <div class="input-group-prepend">
+                                        <div class="input-group-text pr-3" id="btnGroupAddon2"><i class="oi oi-person" style="width: 10px"></i></div>
+                                    </div>
+                                    <input class="form-control py-2 border" type="text" placeholder="Username">
+                                </div>
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <div class="input-group-text pr-3" id="btnGroupAddon2"><i class="oi oi-lock-locked" style="width:10px"></i></div>
+                                    </div>
+                                    <input class="form-control py-2 border" type="password" name="pass" placeholder="Password">
+                                </div>
 
-                    <!-- Register -->
-                    <p>Not a member?
-                        <a href="">Register</a>
-                    </p>
+                            </div>
+                            <button class="btn btn-secondary mb-3" type="submit">Sign in</button>
 
-                </form>
-                <!-- Default form login -->
+                            <!-- Register -->
+                            <p>Not a member?
+                                <a href="">Register</a>
+                            </p>
+
+                        </form>
+                        <!-- Default form login -->
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</div>
-</div>
-</body>
+    </body>
 </html>
 
 
