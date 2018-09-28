ecbf49ea4b3758724094e98a3e4a95085d1991d4
Jonas Wrede <jonaswrede@aol.com>
2018-09-28 12:54:38 +0200

    update module->login->view->login_page_test.php test new symbols for forms (BS input-group) change BS V

--- module/login/view/login_page.php
+++ module/login/view/login_page.php
@@ -59,10 +59,13 @@
                             <!-- Email -->
                             <div>
                                 <div class="input-group mb-2">
-                                    <div class="input-group-prepend">
-                                        <div class="input-group-text pr-3" id="btnGroupAddon2"><i class="oi oi-person" style="width: 10px"></i></div>
+
+                                    <div class="col-xs-2">
+                                        <div class="input-group-prepend">
+                                            <div class="input-group-text pr-3" id="btnGroupAddon2"><i class="oi oi-person" style="width: 10px"></i></div>
+                                        </div>
+                                        <input class="form-control py-2 border" type="text" placeholder="Username">
                                     </div>
-                                    <input class="form-control py-2 border" type="text" placeholder="Username">
                                 </div>
                                 <div class="input-group mb-3">
                                     <div class="input-group-prepend">
