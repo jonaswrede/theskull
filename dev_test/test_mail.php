c45f29c9b3b90fb1fbfb923be60e482da22ee45b
Jonas Wrede <jonaswrede@aol.com>
2018-10-04 10:16:05 +0200

    test mail

---
+++ dev_test/test_mail.php
@@ -0,0 +1,17 @@
+<?php
+$empfaenger = 'jonaswrede@aol.com';
+$betreff = 'Der Betreff';
+$nachricht = 'Hallo';
+$header = array(
+    'From' => 'jonas.wrede@theskull.de',
+    'Reply-To' => 'jonas.wrede@theskull.de',
+    'X-Mailer' => 'PHP/' . phpversion()
+);
+
+if(mail($empfaenger, $betreff, $nachricht, $header)){
+    echo "sucsess";
+}
+else{
+    echo "fail";
+}
+?>
