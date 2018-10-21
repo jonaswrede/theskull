d3bc0104fcc0fadd50668bd9fbd9d2816c07ce0a
Jonas Wrede <jonaswrede@aol.com>
2018-10-21 22:53:28 +0200

    update menu test

---
+++ about.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>about</title>
+</head>
+<body>
+    <div>
+        <?php
+            include getcwd()."template/menu.php";
+        ?>
+    </div>
+
+</body>
+</html>
--- template/menu.php
+++ template/menu.php
@@ -6,11 +6,18 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            // get current URL path and assign 'active' class
+            var pathname = window.location.pathname;
+            $('.nav > li > a[href="'+pathname+'"]').parent().addClass('active');
+        })
+    </script>
 </head>
             <div class="navbar">
                 <div class="navbar-inner">
                     <ul class="nav">
-                        <li><a href="/">Home</a></li>
+                        <li><a href="/index.php">Home</a></li>
                         <li><a href="/about.php">About</a></li>
                         <li><a href="/contact.php">Contact</a></li>
                     </ul>
