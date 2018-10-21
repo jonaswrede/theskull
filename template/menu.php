4d9d5f61460db78a534a014f9ee1e9a6762eed1a
Jonas Wrede <jonaswrede@aol.com>
2018-10-21 22:46:53 +0200

    update menu test

--- template/menu.php
+++ template/menu.php
@@ -7,43 +7,12 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
-<?php
-
-
-public function setMenuStatus($filename){
-    $current_file = basename($_SERVER['REQUEST_URI'], ".php");
-
-    if ($filename == $current_file){
-        echo  '<li class="nav-item active">';
-    }
-    else{
-        echo '<li class="nav-item">';
-    }
-
-}
-?>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-                <?php
-                    setMenuStatus("index")
-                ?>
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <?php
-                setMenuStatus("index")
-                ?>
-                    <a class="nav-link" href="#">Features</a>
-                </li>                <?php
-                setMenuStatus("index")
-                ?>
-                    <a class="nav-link" href="#">Pricing</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+            <div class="navbar">
+                <div class="navbar-inner">
+                    <ul class="nav">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/about.php">About</a></li>
+                        <li><a href="/contact.php">Contact</a></li>
+                    </ul>
+                </div>
 </html>
