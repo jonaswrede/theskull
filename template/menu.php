e0fa54af5980393ed5d353caec1650bdfff72ad0
Jonas Wrede <jonaswrede@aol.com>
2018-10-22 18:50:23 +0200

    update menu a pull right

--- template/menu.php
+++ template/menu.php
@@ -33,7 +33,7 @@
                     <a class="nav-link disabled" href="#">SkuLLList</a>
                 </li>
             </ul>
-            <ul class="nav navbar-nav pull-right">
+            <ul class="nav navbar-nav ml-auto">
                 <li class="nav-item"><a class="nav-link pull-right" href=""> Test</a> </li>
             </ul>
 
