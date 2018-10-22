c25b5baa147d7823ad10cf923baeabee53fa2ec5
Jonas Wrede <jonaswrede@aol.com>
2018-10-22 22:08:35 +0200

    test menu extra loading bootstrap in menu.html

--- template/menu.php
+++ template/menu.php
@@ -14,7 +14,7 @@
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <div class="collapse navbar-collapse-xs" id="navbarNavDropdown">
             <ul class="navbar-nav">
 
                 <li class="nav-item">
