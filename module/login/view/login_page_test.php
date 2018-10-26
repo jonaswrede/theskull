77904ea28f54a56f2bf4309bd4713abcb31a11e1
Jonas Wrede <jonaswrede@aol.com>
2018-10-26 12:41:23 +0200

    login_page_test

--- module/login/view/login_page_test.php
+++ module/login/view/login_page_test.php
@@ -17,16 +17,16 @@ if($_SESSION["login"]){
 <html>
 <head>
     <!-- Latest compiled and minified CSS -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../../ext/open-iconic-master/font/css/open-iconic-bootstrap.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <style>
 
-        html, body{height:100%; margin:0;padding:0; background-color:}
+        html, body{height:100%; margin:0;padding:0}
 
         .container-fluid{
             height:100%;
@@ -34,6 +34,8 @@ if($_SESSION["login"]){
             width: 100%;
             padding: 0;
             /**  border: 5px solid gray;*/
+
+
         }
 
         .row-fluid {height: 100%; display:table-cell; vertical-align: middle;}
@@ -41,12 +43,6 @@ if($_SESSION["login"]){
         .centering {
             float:none;
             margin:0 auto;
-        }
-
-        .center-block {
-            display: table;  /* Instead of display:block */
-            margin-left: auto;
-            margin-right: auto;
         }
     </style>
 
