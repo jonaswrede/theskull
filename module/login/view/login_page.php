91266eb12e24f3e5324a497134932e4ad0590522
Jonas Wrede <jonaswrede@aol.com>
2018-10-29 18:47:02 +0100

    add maximum-scale=1 to login_page

--- module/login/view/login_page.php
+++ module/login/view/login_page.php
@@ -15,7 +15,7 @@ if($_SESSION["login"]){
     <head>
         <!-- Latest compiled and minified CSS -->
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <link rel="stylesheet" href="../../ext/open-iconic-master/font/css/open-iconic-bootstrap.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
