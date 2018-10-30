6fefe3f627fe6c0b61189c8fadf3e4148ba6df58
Jonas Wrede <jonaswrede@aol.com>
2018-10-30 16:54:07 +0100

    update login username placeholder

--- module/login/view/login_page.php
+++ module/login/view/login_page.php
@@ -60,8 +60,9 @@ if($_SESSION["login"]){
                                 <div class="input-group mb-2">
                                     <div class="input-group-prepend">
                                         <div class="input-group-text pr-3" id="btnGroupAddon2"><i class="oi oi-person" style="width: 10px"></i></div>
+
+                                        <input class="form-control py-2 border" type="text" name="username" placeholder="Username">
                                     </div>
-                                    <input class="form-control py-2 border" type="text" name="username" placeholder="Username">
                                 </div>
                                 <div class="input-group mb-3">
                                     <div class="input-group-prepend">
