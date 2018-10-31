8b858522fcd278f3a2cfb4cd83f1be35b21bcf5c
Jonas Wrede <jonaswrede@aol.com>
2018-10-31 11:18:14 +0100

    add mt-2 main container in about.php

--- module/main/view/about.php
+++ module/main/view/about.php
@@ -16,7 +16,7 @@
             include "../../../template/menu.php";
         ?>
     </div>
-    <div class="container">
+    <div class="container mt-2">
         <div class="row">
             <div class="col-12">
                 <div class="text-center">
