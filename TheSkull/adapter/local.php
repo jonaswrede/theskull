3ae5edf7226e89788c70d22732839d2625547c42
Jonas Wrede <jonaswrede@aol.com>
2018-11-21 17:01:26 +0100

    test should not work

--- TheSkull/adapter/local.php
+++ TheSkull/adapter/local.php
@@ -4,5 +4,5 @@ return $connect =
         "host_name" => 'db748804796.db.1and1.com',
         "database" => 'db748804796',
         "user_name" => 'dbo748804796',
-        "password" => 'TSDTml321Bmg()='
+        "password" => 'TSDBTml321Bmg()='
     ];
