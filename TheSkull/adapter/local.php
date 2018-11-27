b7f478bbd96c2c5726167ec7b7b14121a04cc045
Jonas Wrede <jonaswrede@aol.com>
2018-11-27 11:53:22 +0100

    fixed untracked files

--- TheSkull/adapter/local.php
+++
@@ -1,8 +0,0 @@
-<?php
-return $connect =
-    [
-        "host_name" => 'db748804796.db.1and1.com',
-        "database" => 'db748804796',
-        "user_name" => 'dbo748804796',
-        "password" => 'TSDTml321Bmg()='
-    ];
