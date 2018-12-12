b322f094a637e0d24e4808b2f59f1c88e11c8451
Jonas Wrede <jonaswrede@aol.com>
2018-12-12 16:35:12 +0100

    htaccess img size module update

--- TheSkull/module/login/view/login_page.php
+++ TheSkull/module/login/view/login_page.php
@@ -12,6 +12,9 @@ if($_SESSION["login"]){
 ?><!DOCTYPE HTML>
 <html lang="EN">
     <head>
+        <link rel="stylesheet" href="../../ext/open-iconic-master/font/css/open-iconic-bootstrap.css">
+        <link rel="stylesheet" href="../../ext/bootstrap.min.css">
+
         <!-- Latest compiled and minified CSS -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5">
@@ -205,8 +208,6 @@ if($_SESSION["login"]){
                 }, false);
             })();
         </script>
-    <link rel="stylesheet" href="../../ext/open-iconic-master/font/css/open-iconic-bootstrap.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     </body>
 </html>
